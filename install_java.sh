af53200b5cbf59a89b7a738f3ee5551dadb23c84
Sarvjeet Rajvansh <srajvansh1124@gmail.com>
2020-03-09 13:50:07 +0530

    Create install_java.sh

---
+++ install_java.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+apt-get update && apt-get upgrade
+apt-get install vim -y
+
+apt-get install default-jdk -y
+
+ cat << EOF >> /etc/environment
+JAVA_HOME=`readlink -f $(which java)`
+EOF
+
+
+source /etc/environment
+echo $JAVA_HOME
